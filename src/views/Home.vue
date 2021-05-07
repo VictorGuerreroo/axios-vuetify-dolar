55797cc59f35d8b0c89b2eeac2a61370ad47508c
Victor <v.g.m_@hotmail.com>
2021-05-07 12:11:55 +0200

    Finished project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,72 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row :wrap="true">
+      <v-col xs12>
+        <v-card>
+          <v-date-picker
+            v-model="fecha"
+            full-width
+            locale="es"
+            :min="minimo"
+            :max="maximo"
+            @change="getDolar(fecha)"
+          >
+          </v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+            {{ valor }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import axios from "axios";
+import { mapMutations } from 'vuex';
 
-  export default {
-    name: 'Home',
+export default {
+  data() {
+    return {
+      fecha: new Date().toISOString().substr(0, 10),
+      minimo: "1984",
+      maximo: new Date().toISOString().substr(0, 10),
+      valor: null,
+    };
+  },
+  methods: {
+    ...mapMutations(['mostrarLoading', 'ocultarLoading']),
 
-    components: {
-      HelloWorld,
+    async getDolar(dia) {
+          let arrayFecha = dia.split(["-"]);
+          let ddmmyy = arrayFecha[2] + "-" + arrayFecha[1] + "-" + arrayFecha[0];
+      try {
+        
+          this.mostrarLoading({titulo: 'Accediendo a información', color: 'secondary'})
+
+          let datos = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`);
+          
+          if(datos.data.serie.length > 0) {
+              this.valor = await datos.data.serie[0].valor;
+          }else{
+            this.valor ='Sin resultados'
+          }
+          
+          } 
+      catch (error) 
+         {
+          console.log(error);
+         } 
+      finally 
+         {
+          this.ocultarLoading()
+         }
     },
-  }
+  },
+  created() {
+    this.getDolar(this.fecha);
+  },
+};
 </script>
